2a5372b7b176660bb61af6b7742c34bb47d8984b
Rafał Krupiński <krupinski.rafal@gmail.com>
2025-01-06 18:25:30 +0100

    Batman!

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Fabrice](https://github.com/callstackincubator/fabrice-ai) playground
+
+- `cp .env.example .env`
+- `npm install`
+- `npm start -- "I'm trying to wrap my head around this ambiguous function call"`
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { agent } from 'fabrice-ai/agent';
+import { teamwork } from 'fabrice-ai/teamwork';
+import { workflow } from 'fabrice-ai/workflow';
+import { solution } from 'fabrice-ai/solution';
+
+import 'dotenv/config';
+
+const translator = agent({
+  description: 'You are a skilled translator.',
+});
+
+const technicalWriter = agent({
+  description: 'You are a skilled technical writer.',
+});
+
+const wf = workflow({
+  team: { translator, technicalWriter },
+  knowledge: `
+    Original text:
+    
+    "${process.argv[2]}"
+  `,
+  description: `
+    Please review the grammar, style, and overall accuracy. Suggest improvements
+    while keeping modifications to the source minimal.
+  `,
+  output: `
+    A revised version of the original text followed by a list of changes.
+ `,
+});
+
+console.log(solution(await teamwork(wf)));
